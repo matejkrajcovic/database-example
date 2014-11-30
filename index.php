63718ed52bede981efba70a42c8011288fff66da
Matej Krajčovič <elbarto512@gmail.com>
2014-11-30 23:28:47 +0100

    Add tasks 2 and 3

--- index.php
+++ index.php
@@ -12,9 +12,43 @@ $dbconn = pg_connect(sprintf("host=%s dbname=%s user=%s password=%s", $server, $
 $query = file_get_contents("query1.sql");
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-echo "<h1>Prva uloha<h2>\n";
+echo "<h1>Prvá uloha</h1>\n";
 
-echo "<table>\n";
+echo "<table border=\"2\">\n<tr><th>Meno</th><th>Počet podriadených</th><th>Priemerný plat</th></tr>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+pg_free_result($result);
+
+$query = file_get_contents("query2.sql");
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+echo "<h1>Druhá uloha</h1>\n";
+
+echo "<table border=\"2\">\n<tr><th>Oddelenie</th><th>Platový stupeň</th><th>Počet zamestnancov</th></tr>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+pg_free_result($result);
+
+$query = file_get_contents("query3.sql");
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+echo "<h1>Prvá uloha</h1>\n";
+
+echo "<table border=\"2\">\n<tr><th>Platový stupeň</th><th>Maximálny plat</th><th>Počet zamestnancov</th><th>Celkové náklady</th><th>Priemerné zvýšenie</th></tr>\n";
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     echo "\t<tr>\n";
     foreach ($line as $col_value) {
